1998a22919631c0800b987d46c5fe254a01d092a
Kevin <kevinkzc15@gmail.com>
2023-06-24 11:58:28 +0800

    Initial Commit

---
+++ The useless app/The_useless_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_useless_appApp.swift
+//  The useless app
+//
+//  Created by Kevin De Baboon on 24/6/23.
+//
+
+import SwiftUI
+
+@main
+struct The_useless_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
